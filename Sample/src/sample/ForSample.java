1b51526ff393e10969ee4b055da3dda0f2969614
"soshi.kogure" <soshi.kogure.d464@dirbato.co.jp>
2025-05-23 14:10:05 +0900

    Chapter7

---
+++ Sample/src/sample/ForSample.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class ForSample {
+
+    public static void main(String[] args) {
+        var numbers = new int[] {1, 2, 3, 4, 5};
+
+        for (var number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
